d45e89ae861705d15d77f46d9ab4980115ff511b
quint <quint@137.125.130.139>
2022-09-29 10:14:57 -0400

    Added GPA field and implemented getter and setter for field

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -10,6 +10,7 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student extends Human {
+    private int GPA;
 
     @Override
     public String getAddress() {
@@ -21,6 +22,8 @@ public class Student extends Human {
         this.address = address;
     }
     
+    
+    
 	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
@@ -28,5 +31,13 @@ public class Student extends Human {
 	// ToDo 3: Add a field for GPA and create setter and getter
 	
 	// ToDo 4: Add comments to your code
+
+    public int getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(int GPA) {
+        this.GPA = GPA;
+    }
 
 }
