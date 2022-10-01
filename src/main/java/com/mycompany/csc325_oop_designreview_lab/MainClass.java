f528c063d0afe7440078062ea65e144066ffedbd
quint <quint@137.125.130.139>
2022-09-30 23:17:48 -0400

    Added Javadoc and single line comments

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author MoaathAlrajab
+ * @author JeovaniQuintanilla
  */
 public class MainClass {
 	
@@ -16,21 +16,18 @@ public class MainClass {
                 
                 // ToDo 8: The senior class should have a minimum of 85 credits  
 		
-		Student std1 = new Freshman("James", 20, 12);// name, age, credits
-                Student std2 = new Senior("John", 30, 90);
-                Scanner user = new Scanner(System.in);
-                double fGpa = user.nextDouble();
-		double sGpa = user.nextDouble();
+		Student std1 = new Freshman("James", 20, 12); //polymorphism using freshman with student
+                Student std2 = new Senior("John", 30, 90); //polymorphism using senior with student
+                Scanner user = new Scanner(System.in);//scanner to allow user to input GPA
+                double fGpa = user.nextDouble();// GPA for freshmen
+		double sGpa = user.nextDouble();//GPA for Senior
                 
-                std1.setGPA(fGpa);
-                std2.setGPA(sGpa);
+                std1.setGPA(fGpa);//sets freshmen GPA
+                std2.setGPA(sGpa);//sets Senior Gpa
                 
-		System.out.println(std1);
-                
-                System.out.println(std2);
-		
-		// ToDo 12: add comments and explain your code
-		
+		System.out.println(std1.toString());//prints Freshman data
+                System.out.println(std2.toString());//prints Senior data
+	
 		// ToDo 13: submit using a pull request.
 	}
 
