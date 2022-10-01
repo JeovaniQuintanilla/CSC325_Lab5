f398be18207b7c779cae4b146c104f0f69359f7f
quint <quint@137.125.130.139>
2022-09-30 20:18:07 -0400

    changed data type of age from short to int

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -13,16 +13,13 @@ public class MainClass {
 	public static void main(String[] args) {
 		// ToDo 5: Fix the error
 		
-		// ToDo 6: Fix the constructor of Student class
+		
                 
-                // Todo 7: Create two classes for Freshman and Senior 
+              
                 
                 // ToDo 8: The senior class should have a minimum of 85 credits  
 		
-		// ToDo 9: Add a toString method for Freshman class
 		
-                // ToDo 10: Add a toString method for Senior class
-                
 		Freshman std1= new Student("James", 20, 12); // name, age, credits
                 
                 Senior std2 = new Student("John", 30, 90);
